6603e513fe8aa9961c702b2bff059674686a1a0e
ttatiana18 <61387311+ttatiana18@users.noreply.github.com>
2022-03-18 09:02:34 -0500

    cambios en registro

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('dni');
             $table->string('name');
+            $table->string('surname');
+            $table->date('birth_date');
+            $table->string('birth_place');
+            $table->string('address');
+            $table->string('gender');
+            $table->string('profile_picture');
+            $table->string('role');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- resources/views/layouts/external/layout.blade.php
+++ resources/views/layouts/external/layout.blade.php
@@ -1,1 +1,60 @@
-hola q tal
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Tailwind is included -->
+    <link rel="stylesheet" href="{{ asset('css/external.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    @livewireStyles
+
+    @livewireScripts
+
+</head>
+
+<body>
+
+    <div id="app">
+        @include('layouts.dashboard.navigation')
+
+        <section class="is-title-bar">
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
+                @yield('header')
+            </div>
+        </section>
+
+
+
+
+        <section class="section main-section">
+            @yield('content')
+        </section>
+    </div>
+
+
+    <!-- Icons below are for demo only. Feel free to use any icon pack. Docs: https://bulma.io/documentation/elements/icon/ -->
+    {{-- <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css"> --}}
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css">
+
+    <script>
+        Livewire.on('closeModal', () => {
+            const event = new Event('closeme')
+            window.dispatchEvent(event)
+        })
+    </script>
+
+    @stack('scripts')
+
+
+</body>
+
+</html>
+
