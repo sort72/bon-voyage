dce8072d9989cd673c8b0ed9737ff5e3d2b59fc3
sort72 <saoa814@gmail.com>
2022-03-18 10:37:53 -0500

    Add CheckRole middleware. Improve dashboard menu

---
+++ resources/views/pages/dashboard/destination/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.dashboard.layout')
+
+@section('header', 'Destinos')
+
+@section('content')
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                Fo
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
