1db57da8752b68a2d286a3ada446549d89268ef5
sort72 <saoa814@gmail.com>
2022-03-30 08:26:30 -0500

    Add flight index

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Flight extends Model
 {
     use HasFactory;
+
+    public function destination()
+    {
+        return $this->belongsTo(Destination::class, 'destination_id');
+    }
+
+    public function origin()
+    {
+        return $this->belongsTo(Destination::class, 'origin_id');
+    }
 }
