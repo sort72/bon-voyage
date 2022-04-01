db5f5b8abe34750cc40ee80c5ff2abaf6956bf28
sort72 <saoa814@gmail.com>
2022-04-01 10:33:34 -0500

    Improvements on register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,13 +15,13 @@
             <div class="flex">
                 <!-- Name -->
                 <div class="w-1/2 px-2">
-                    <x-label for="name" :value="__('Nombres')" />
+                    <x-label for="name" value="Nombres *" />
 
                     <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
                 </div>
 
                 <div class="w-1/2 px-2">
-                    <x-label for="surname" :value="__('Apellidos')" />
+                    <x-label for="surname" value="Apellidos *" />
 
                     <x-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')" required autofocus />
                 </div>
@@ -29,13 +29,13 @@
             <div class="flex mt-4">
                 <!-- Email Address -->
                 <div class="w-1/2 px-2">
-                    <x-label for="email" :value="__('Email')" />
+                    <x-label for="email" value="Email *" />
 
                     <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
                 </div>
 
                 <div class="w-1/2 px-2">
-                    <x-label for="dni" :value="__('Documento')" />
+                    <x-label for="dni" value="Documento *" />
 
                     <x-input id="dni" class="block mt-1 w-full" type="text" name="dni" :value="old('dni')" required />
                 </div>
@@ -43,20 +43,20 @@
             <div class="flex mt-4">
                 <!-- Email Address -->
                 <div class="w-1/2 px-2">
-                    <x-label for="birth_date" :value="__('Fecha de nacimiento')" />
+                    <x-label for="birth_date" value="Fecha de nacimiento *" />
 
                     <x-input id="birth_date" class="block mt-1 w-full" type="date" name="birth_date" :value="old('birth_date')" required />
                 </div>
 
                 <div class="w-1/2 px-2">
-                    <x-label for="birth_place" :value="__('Lugar de nacimiento')" />
+                    <x-label for="birth_place" value="Lugar de nacimiento *" />
                     <x-input id="birth_place" class="block mt-1 w-full" type="text" name="birth_place" :value="old('birth_place')" required />
                 </div>
             </div>
             <div class="flex mt-4">
                 <!-- Email Address -->
                 <div class="w-1/2 px-2">
-                    <x-label for="gender" :value="__('Género')" />
+                    <x-label for="gender" value="Género" />
 
                     <select name="gender" id="gender" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-sky-300 focus:ring focus:ring-sky-200 focus:ring-opacity-50">
                         <option value="">Seleccione el género</option>
@@ -67,7 +67,7 @@
                 </div>
 
                 <div class="w-1/2 px-2">
-                    <x-label for="address" :value="__('Dirección de facturación')" />
+                    <x-label for="address" value="Dirección de facturación *" />
                     <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required />
                 </div>
             </div>
@@ -75,7 +75,7 @@
             <div class="flex mt-4">
                 <!-- Password -->
                 <div class="w-1/2 px-2">
-                    <x-label for="password" :value="__('Password')" />
+                    <x-label for="password" value="Password *" />
 
                     <x-input id="password" class="block mt-1 w-full"
                                     type="password"
@@ -85,13 +85,16 @@
 
                 <!-- Confirm Password -->
                 <div class="w-1/2 px-2">
-                    <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                    <x-label for="password_confirmation" value="Confirm Password *" />
 
                     <x-input id="password_confirmation" class="block mt-1 w-full"
                                     type="password"
                                     name="password_confirmation" required />
                 </div>
             </div>
+            <p class="italic my-2 text-sm px-2">
+                Todos los campos marcados con * son obligatorios.
+            </p>
             <div class="flex justify-center mt-4">
                 <a class="underline text-sm text-sky-500 hover:text-sky-700" href="{{ route('login') }}">
                     {{ __('Ya tienes una cuenta? Inicia sesión') }}
